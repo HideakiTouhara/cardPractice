54bbe265bcd26a5f1879258b5d8c0dad98451259
HideakiTouhara <hideee6723@gmail.com>
2018-02-25 11:13:34 +0900

    Create TinderCardView.swift

---
+++ TinderCard/TinderCardView.swift
@@ -0,0 +1,13 @@
+//
+//  TinderCardView.swift
+//  TinderCard
+//
+//  Created by HideakiTouhara on 2018/02/25.
+//  Copyright © 2018年 HideakiTouhara. All rights reserved.
+//
+
+import Foundation
+
+open class TinderCardView: UIView {
+    
+}
