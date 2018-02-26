b37621941d7ac794ce6417973c4fe2793b87ca7c
HideakiTouhara <hideee6723@gmail.com>
2018-02-26 12:48:34 +0900

    Add panGesture

--- TinderCard/TinderCardView.swift
+++ TinderCard/TinderCardView.swift
@@ -8,6 +8,26 @@
 
 import Foundation
 
+public protocol TinderCardViewDataSource: class {
+    func numberOfCards(_ tinderCard: TinderCardView) -> Int
+    func tinderCard(_ tinderCard: TinderCardView, viewForCardAt index: Int) -> UIView
+}
+
 open class TinderCardView: UIView {
     
+    public weak var dataSource: TinderCardViewDataSource? {
+        didSet {
+            setUp()
+        }
+    }
+    private func setUp() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture(_:)))
+        self.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func panGesture(_ sender: UIPanGestureRecognizer) {
+        print("pan")
+        let location = sender.translation(in: self)
+        self.center = CGPoint(x: self.center.x + location.x, y: self.center.y + location.y)
+    }
 }
